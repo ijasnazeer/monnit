74b29d7a2ba8c46f3e0f5f44543585461a2773e8
ijasnazeer <ijasnazeer786@gmail.com>
2023-11-05 23:17:57 +0530

    employee--attendance style completed

--- components/Attendance/Attendance.js
+++ components/Attendance/Attendance.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import './style.css'
+import { Calendar } from "@/components/ui/calendar"
 
-export default function Attendance() {
-  return (
+
+export default function Attendance() {  
+    const [employeeData, setEmployeeData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    // Load employee data from the specified URL
+    fetch('http://localhost:3006/employees')
+      .then((response) => response.json())
+      .then((data) => {
+        setEmployeeData(data);
+        setLoading(false);
+        console.log(data[0].name) //last
+      })
+      .catch((error) => {
+        console.error('Error loading employee data:', error);
+        setLoading(false);
+      });
+  }, []);
+    
+    return (
     <div className='attendance-component'>
         <div className='head'>
             <div>
                 <span>Attendance record</span>
-                <div className='right-side'>
-                    <div className='paging'>
-                        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M15.0005 17.7902C15.3905 17.4002 15.3905 16.7702 15.0005 16.3802L11.1205 12.5002L15.0005 8.62022C15.3905 8.23022 15.3905 7.60022 15.0005 7.21022C14.6105 6.82022 13.9805 6.82022 13.5905 7.21022L9.00047 11.8002C8.61047 12.1902 8.61047 12.8202 9.00047 13.2102L13.5905 17.8002C13.9705 18.1802 14.6105 18.1802 15.0005 17.7902Z" fill="#ABB6C7"/>
-                        </svg>
-                        June 2022
-                        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M8.99953 7.20978C8.60953 7.59978 8.60953 8.22978 8.99953 8.61978L12.8795 12.4998L8.99953 16.3798C8.60953 16.7698 8.60953 17.3998 8.99953 17.7898C9.38953 18.1798 10.0195 18.1798 10.4095 17.7898L14.9995 13.1998C15.3895 12.8098 15.3895 12.1798 14.9995 11.7898L10.4095 7.19978C10.0295 6.81978 9.38953 6.81978 8.99953 7.20978Z" fill="#ABB6C7"/>
-                        </svg>
-                    </div>
                     <div className='page-balls'>
                         <svg width="26" height="27" viewBox="0 0 26 27" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="13" cy="13.5" r="13" fill="#05A24D"/>
@@ -43,13 +54,50 @@ export default function Attendance() {
                         <path d="M13.4813 19.66C12.5533 19.66 11.7479 19.5213 11.0653 19.244C10.3826 18.9667 9.85458 18.5773 9.48125 18.076C9.10792 17.564 8.92125 16.9667 8.92125 16.284C8.92125 15.6013 9.10258 15.0253 9.46525 14.556C9.82792 14.0867 10.3506 13.7293 11.0333 13.484C11.7159 13.2387 12.5319 13.116 13.4813 13.116C14.4306 13.116 15.2466 13.2387 15.9293 13.484C16.6226 13.7293 17.1506 14.092 17.5133 14.572C17.8866 15.0413 18.0733 15.612 18.0733 16.284C18.0733 16.9667 17.8813 17.564 17.4973 18.076C17.1239 18.5773 16.5906 18.9667 15.8973 19.244C15.2146 19.5213 14.4093 19.66 13.4813 19.66ZM13.4813 18.028C14.2493 18.028 14.8573 17.868 15.3053 17.548C15.7533 17.228 15.9773 16.7853 15.9773 16.22C15.9773 15.6653 15.7533 15.228 15.3053 14.908C14.8573 14.588 14.2493 14.428 13.4813 14.428C12.7133 14.428 12.1106 14.588 11.6733 14.908C11.2359 15.228 11.0173 15.6653 11.0173 16.22C11.0173 16.7853 11.2359 17.228 11.6733 17.548C12.1106 17.868 12.7133 18.028 13.4813 18.028ZM13.4813 14.204C12.6173 14.204 11.8759 14.092 11.2573 13.868C10.6386 13.6333 10.1586 13.2973 9.81725 12.86C9.48658 12.412 9.32125 11.8787 9.32125 11.26C9.32125 10.62 9.49192 10.0707 9.83325 9.612C10.1853 9.14267 10.6759 8.78 11.3053 8.524C11.9346 8.268 12.6599 8.14 13.4813 8.14C14.3133 8.14 15.0439 8.268 15.6733 8.524C16.3026 8.78 16.7933 9.14267 17.1453 9.612C17.4973 10.0707 17.6733 10.62 17.6733 11.26C17.6733 11.8787 17.5026 12.412 17.1613 12.86C16.8306 13.2973 16.3506 13.6333 15.7213 13.868C15.0919 14.092 14.3453 14.204 13.4813 14.204ZM13.4813 12.924C14.1426 12.924 14.6653 12.7853 15.0493 12.508C15.4333 12.22 15.6253 11.836 15.6253 11.356C15.6253 10.8547 15.4279 10.4653 15.0333 10.188C14.6386 9.91067 14.1213 9.772 13.4813 9.772C12.8413 9.772 12.3293 9.91067 11.9453 10.188C11.5613 10.4653 11.3693 10.8547 11.3693 11.356C11.3693 11.836 11.5559 12.22 11.9293 12.508C12.3133 12.7853 12.8306 12.924 13.4813 12.924Z" fill="#000F28"/>
                         </svg>
                     </div>
-                </div>
             </div>
         </div>
 
 
 
-        <div className='body'>hi</div>
+        <div className='body'>
+
+          {loading ? (
+          // Display a loading indicator if data is still loading
+          <p>Loading...</p>
+        ) : (
+          // Render the Calendar component with the loaded employee data
+          <Calendar
+            mode="single"
+            // Pass employeeData to the Calendar component
+            data={employeeData}
+            className="rounded-md border calender"
+          />
+        )}
+
+<div className="sasi">
+  <h2>Employee Data</h2>
+  {employeeData && employeeData.employees && employeeData.employees.length > 0 ? (
+    employeeData.employees.map((employee, index) => (
+      <div key={employee.employee_id}>
+        <p>Employee ID: {employee.employee_id}</p>
+        <p>Name: {employee.name}</p>
+        <p>Attendance:</p>
+        <ul>
+          {employee.attendance.map((attendanceEntry, entryIndex) => (
+            <li key={entryIndex}>
+              Date: {attendanceEntry.date}, Status: {attendanceEntry.status}
+            </li>
+          ))}
+        </ul>
+      </div>
+    ))
+  ) : (
+    <p>No employee data available....</p>
+  )}
+</div>
+
+
+        </div>
     </div>
   )
 }
